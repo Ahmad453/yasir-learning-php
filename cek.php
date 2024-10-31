5b3c3b087cf1dbcd32ca8915c4acd253b1e9daa5
ahmad yasir al fadhil <80280499+Ahmad453@users.noreply.github.com>
2024-10-31 10:43:45 +0700

    Create cek.php
    tambahan baru

---
+++ cek.php
@@ -0,0 +1,51 @@
+<!DOCKTYPE html>
+<html>
+  <head>
+    <title>Pendaftaran</title>
+  </head>
+  <body>
+    <?php
+$tombol = "<a href='index.php'>kembali</a>";
+if($_POST['nama'] == "" || $_POST['email'] == "" || $_POST['alamat'] == ""){
+
+  if($_POST['nama'] == "")
+    echo "ngaran kudu di isi. <br>";
+}
+  if($_POST['email'] == "")
+    echo "email kudu di isi. <br>";
+}
+if($_POST['alamat'] == "")
+    echo "alamat kudu di isi. <br>";
+}
+echo $tombol;
+
+}else{
+  ?>
+      <h2> Masukan data diri anda</h2>
+      <table border="1">
+        <tr>
+          <td>Nama</td>
+          <td><?php echo $_POST['nama']; ?></td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td><?php echo $_POST['email']; ?></td>
+        </tr>
+        <tr>
+          <td>Jenis Kelamin</td>
+          <td><?php echo $_POST['jenis_kelamin']; ?></td>
+        </tr>
+        <tr>
+          <td>Alamat</td>
+          <td><?php echo $_POST['alamat']; ?></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><a href="index.php">Kembali</a></td>
+        </tr>
+      </table>
+      <?php
+}
+?>
+  </body>
+</html>
